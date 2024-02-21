8b0d07f04537473b5ac4283c3e12ada5dcb95962
Alexis Vannson <147932138+alexisvannson@users.noreply.github.com>
2024-02-21 21:53:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -77,8 +77,8 @@ sequential_model.compile(optimizer='adam',
                          metrics=['accuracy'])
 
 # Train the model
-# sequential_model.fit(X_train_features['pixel_values'], y_train, epochs=10, validation_data=(X_val_features['pixel_values'], y_val))
+ sequential_model.fit(X_train_features['pixel_values'], y_train, epochs=10, validation_data=(X_val_features['pixel_values'], y_val))
 
 # Evaluate the model
-# loss, accuracy = sequential_model.evaluate(X_test_features['pixel_values'], y_test)
-# print(f'Test Loss: {loss}, Test Accuracy: {accuracy}')
+ loss, accuracy = sequential_model.evaluate(X_test_features['pixel_values'], y_test)
+ print(f'Test Loss: {loss}, Test Accuracy: {accuracy}')
